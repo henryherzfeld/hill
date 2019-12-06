6c85a8feb8822e5d7a966454807c4c63a9d958ae
Henry Herzfeld <herzfeld2@gmail.com>
2019-12-06 11:40:19 -0500

    add frame saving fx, directory management

--- labeling.py
+++ labeling.py
@@ -2,100 +2,162 @@ import cv2
 import time
 import os
 import numpy as np
+import shutil
+
+raw_frames_path = 'F:/hill/frames'
+output_folder = 'labeled'
+
+_, _, filenames = next(os.walk(raw_frames_path))
+
+n_pitch_frames = 15
+post_save_buffer = 10
+save_buffer_count = post_save_buffer
+saved = False
+label = True
+
+if label:
+    output_path = os.join(os.getcwd(), output_folder)
+
+    if not os.path.isdir(output_path):
+        os.mkdir(output_folder)
+
+def save_pitch_frames(label, seq_end_idx):
+
+    frames = filenames[seq_end_idx - n_pitch_frames:seq_end_idx]
+
+    save_path = os.join.path(output_path, label)
+
+    os.mkdirs(save_path)
+
+    for frame in frames:
+        source_path = os.path.join(raw_frames_path, frame)
+        destination_path = os.path.join(save_path, frame)
+
+        shutil.copyfile(source_path, save_path)
+
+        return True
 
 draw = True
+view = True
+
+uncertain_frames = 0
+uncertain_frame_threshold = 3
+
 threshold = .99
 strike_pattern_frame_label = 144
 ball_pattern_frame_label = 724
 box_pattern_frame_label = 144
 r2_pattern_frame_label = 9
 
-raw_frames_path = 'F:/hill/frames'
-output_path = 'F:/hill/labeled'
+cropx = 500
+cropy = 100
+w, h = img.shape[0:2] # assumes every frame is the same size
 
-_, _, filenames = next(os.walk(raw_frames_path))
+ball_count = 1
+strike_count = 1
 
-box_pattern_frame_path = os.path.join(raw_frames_path, filenames[box_pattern_frame_label])
-box_pattern_frame = cv2.imread(box_pattern_frame_path)
+# reading in and cropping templates from frames
+box_pattern_frame = cv2.imread(os.path.join(raw_frames_path, filenames[box_pattern_frame_label]))
 box_pattern = box_pattern_frame[332:338, 770:1145]
+box_h, box_w = box_pattern.shape[0:2]
 
-strike_pattern_frame_path = os.path.join(raw_frames_path, filenames[strike_pattern_frame_label])
-strike_pattern_frame = cv2.imread(strike_pattern_frame_path)
+strike_pattern_frame = cv2.imread(os.path.join(raw_frames_path, filenames[strike_pattern_frame_label]))
 strike_pattern = strike_pattern_frame[553:592, 1030:1073]
+strike_h, strike_w = strike_pattern.shape[0:2]
 
-ball_pattern_frame_path = os.path.join(raw_frames_path, filenames[ball_pattern_frame_label])
-ball_pattern_frame = cv2.imread(ball_pattern_frame_path)
+ball_pattern_frame = cv2.imread(os.path.join(raw_frames_path, filenames[ball_pattern_frame_label]))
 ball_pattern = ball_pattern_frame[450:495, 1220:1262]
+ball_h, ball_w = ball_pattern.shape[0:2]
 
-r2_pattern_frane_path = os.path.join(raw_frames_path, filenames[r2_pattern_frame_label])
-r2_pattern_frame = cv2.imread(r2_pattern_frane_path)
+r2_pattern_frame = cv2.imread(os.path.join(raw_frames_path, filenames[r2_pattern_frame_label]))
 r2_pattern = r2_pattern_frame[610:630, 1220:1245]
+r2_h, r2_w = r2_pattern.shape[0:2]
 
-# viewing the patterns
-cv2.imshow('box,', box_pattern)
-cv2.imshow('strike', strike_pattern)
-cv2.imshow('ball', ball_pattern)
-cv2.imshow('r2', r2_pattern)
-cv2.waitKey()
+# viewing the templates
+if view:
+    cv2.imshow('box,', box_pattern)
+    cv2.imshow('strike', strike_pattern)
+    cv2.imshow('ball', ball_pattern)
+    cv2.imshow('r2', r2_pattern)
+    cv2.waitKey()
 
 for i, file in enumerate(filenames):
 
-    frame_path = os.path.join(raw_frames_path, file)
+    if not save_buffer_count:
+        save_buffer_count = post_save_buffer
+        saved = False
 
-    print(frame_path)
-    img = cv2.imread(frame_path)
+    if saved:
+        save_buffer_count -= 1
 
-    w, h = img.shape[0:2]
+    else:
+        frame_path = os.path.join(raw_frames_path, file)
+        img = cv2.imread(frame_path)
 
-    cropx = 500
-    cropy = 100
-    img = img[50+cropy:w-50, 0+cropx:h-cropx]
+        # cropping image
+        img = img[50+cropy:w-50, 0+cropx:h-cropx]
 
-    box_res = cv2.matchTemplate(img, box_pattern, cv2.TM_CCOEFF_NORMED)
-    box_loc = np.where(box_res >= threshold)
+        print(frame_path)
 
-    if box_loc:
-        box_h, box_w = box_pattern.shape[0:2]
+        # template matching for box pattern
+        box_res = cv2.matchTemplate(img, box_pattern, cv2.TM_CCOEFF_NORMED)
+        box_loc = np.where(box_res >= threshold)
 
-        if draw:
-            for pt in zip(*box_loc[::-1]):
-                cv2.rectangle(img, pt, (pt[0] +box_w, pt[1] + box_h), (0, 0, 255), 2)
-
-        r2_res = cv2.matchTemplate(img, r2_pattern, cv2.TM_CCOEFF_NORMED)
-        r2_loc = np.where(r2_res >= threshold)
-
-        if not r2_loc:
-
-            strike_res = cv2.matchTemplate(img, strike_pattern, cv2.TM_CCOEFF_NORMED)
-            ball_res = cv2.matchTemplate(img, ball_pattern, cv2.TM_CCOEFF_NORMED)
-
-            w, h = strike_pattern.shape[0:2]
-
-            strike_loc = np.where(strike_res >= threshold)
-            ball_loc = np.where(ball_res >= threshold)
-
-            if strike_loc:
-                name = "strike"
-            elif ball_loc:
-                name = "ball"
-
-            if draw:
-                for pt in zip(*strike_loc[::-1]):
-                    print("strike")
-                    cv2.rectangle(img, pt, (pt[0] + w, pt[1] + h), (0, 0, 255), 2)
-
-                for pt in zip(*ball_loc[::-1]):
-                    print("ball")
-                    cv2.rectangle(img, pt, (pt[0] + w, pt[1] + h), (0, 0, 255), 2)
-
-        else:
+        if box_loc:
             if draw:
                 for pt in zip(*box_loc[::-1]):
-                    print("r2")
-                    w, h = r2_pattern.shape[0:2]
-                    cv2.rectangle(img, pt, (pt[0] + w, pt[1] + h), (0, 0, 255), 2)
+                    cv2.rectangle(img, pt, (pt[0] + box_w, pt[1] + box_h), (0, 0, 255), 2)
 
-        cv2.imshow('image', img)
+            # template matching for r2 pattern
+            r2_res = cv2.matchTemplate(img, r2_pattern, cv2.TM_CCOEFF_NORMED)
+            r2_loc = np.where(r2_res >= threshold)
 
-        cv2.waitKey()
+            if not r2_loc:
+                # template matching for strike pattern
+                strike_res = cv2.matchTemplate(img, strike_pattern, cv2.TM_CCOEFF_NORMED)
+                strike_loc = np.where(strike_res >= threshold)
 
+                if not strike_loc:
+                    # template matching for ball pattern
+                    ball_res = cv2.matchTemplate(img, ball_pattern, cv2.TM_CCOEFF_NORMED)
+                    ball_loc = np.where(ball_res >= threshold)
+
+                    if not ball_loc:
+                        uncertain_frames += 1
+
+                        if uncertain_frames >= uncertain_frame_threshold:
+                            name = "ball"
+                            saved = save_pitch_frames(name + ball_count, i - uncertain_frames)
+                            ball_count += 1
+                            uncertain_frames = 0
+
+                    else:
+                        name = "ball"
+                        saved = save_pitch_frames(name + ball_count, i - uncertain_frames)
+                        ball_count += 1
+                        uncertain_frames = 0
+
+                else:
+                    name = "strike"
+                    saved = save_pitch_frames(name + strike_count, i - uncertain_frames)
+                    uncertain_frames = 0
+
+
+                if draw:
+                    for pt in zip(*strike_loc[::-1]):
+                        print("strike")
+                        cv2.rectangle(img, pt, (pt[0] + strike_w, pt[1] + strike_h), (0, 0, 255), 2)
+
+                    for pt in zip(*ball_loc[::-1]):
+                        print("ball")
+                        cv2.rectangle(img, pt, (pt[0] + ball_w, pt[1] + ball_h), (0, 0, 255), 2)
+
+            else:
+                if draw:
+                    for pt in zip(*r2_loc[::-1]):
+                        print("r2")
+                        cv2.rectangle(img, pt, (pt[0] + r2_w, pt[1] + r2_h), (0, 0, 255), 2)
+
+            if view:
+                cv2.imshow('image', img)
+                cv2.waitKey()
